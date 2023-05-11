7204d7e1792fe709fd3649a0435ba3eaec097e21
Pascal <18620961+Courtsilius@users.noreply.github.com>
2023-05-11 15:40:45 +0200

    disallow empty strings to be added with helper func

--- src/helper.rs
+++ src/helper.rs
@@ -1,7 +1,7 @@
 pub fn add(vec: &mut Vec<String>, word: String) {
     let is_present = vec.iter().any(|w| *(w) == word);
 
-    if !is_present {
+    if !is_present && word.len() > 0 {
         vec.push(word);
     }
 }
