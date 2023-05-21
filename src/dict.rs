5cea6ad467cf9781bdf219c0ccbac7f1a074b499
Pascal <courtsilius@protonmail.com>
2023-05-22 00:16:02 +0200

    refactoring + fmt

--- src/dict.rs
+++ src/dict.rs
@@ -8,8 +8,16 @@ pub mod dict_mod {
 
     pub fn get_language(s: String) -> Language {
         match s.as_str() {
-            "en" => Language::new("en".to_string(), "Englisch".to_string(), "English".to_string()),
-            "de" => Language::new("de".to_string(), "Deutsch".to_string(), "German".to_string()),
+            "en" => Language::new(
+                "en".to_string(),
+                "Englisch".to_string(),
+                "English".to_string(),
+            ),
+            "de" => Language::new(
+                "de".to_string(),
+                "Deutsch".to_string(),
+                "German".to_string(),
+            ),
             _ => panic!("No valid language found."),
         }
     }
@@ -32,11 +40,8 @@ pub mod dict_mod {
         scraper::Html::parse_document(response)
     }
 
-    fn string_index(source: &String, pattern: &String) -> Vec<usize> {
-        source
-            .match_indices(pattern)
-            .map(|(i, _)| i)
-            .collect()
+    fn string_indices(source: &str, pattern: &String) -> Vec<usize> {
+        source.match_indices(pattern).map(|(i, _)| i).collect()
     }
 
     fn filter(document: Html, translation_request: TranslationRequest) -> Translation {
@@ -47,20 +52,19 @@ pub mod dict_mod {
             lang_lines.push_str(&item);
         });
 
-        let mut v_f = string_index(&lang_lines, translation_request.from().name());
+        let mut v_f = string_indices(&lang_lines, translation_request.from().name());
         let mut from_index: &usize = v_f.first().unwrap_or(&0);
 
-        let mut v_t = string_index(&lang_lines, translation_request.to().name());
+        let mut v_t = string_indices(&lang_lines, translation_request.to().name());
         let mut to_index: &usize = v_t.first().unwrap_or(&0);
 
-
         if from_index == &0 {
-            v_f = string_index(&lang_lines, translation_request.from().alt_name());
+            v_f = string_indices(&lang_lines, translation_request.from().alt_name());
             from_index = v_f.first().unwrap_or(&0);
         }
 
         if to_index == &0 {
-            v_t = string_index(&lang_lines, translation_request.to().alt_name());
+            v_t = string_indices(&lang_lines, translation_request.to().alt_name());
             to_index = v_t.first().unwrap_or(&0);
         }
 
@@ -71,26 +75,24 @@ pub mod dict_mod {
 
         let title_selector = scraper::Selector::parse("td.td7nl").unwrap();
 
-        let mut lines = document.select(&title_selector).map(|x| x.inner_html());
+        let lines = document.select(&title_selector).map(|x| x.inner_html());
         let mut all: String = "".to_owned();
         lines.zip(1..1000).for_each(|(item, _number)| {
             all.push_str(&item);
         });
 
-        let v_max = string_index(&all, &"2".to_string());
-        let max_index: &usize = v_max.first().unwrap_or(&0);
-
+        let v_max = string_indices(&all, &"2".to_string());
+        let mut max_index: &usize = v_max.first().unwrap_or(&0);
 
         if max_index == &0 {
-            v_f = string_index(&all, &"Partial Matches".to_string());
-            from_index = v_f.first().unwrap_or(&0);
+            v_f = string_indices(&all, &"Partial Matches".to_string());
+            max_index = v_f.first().unwrap_or(&0);
         }
-
 
         let re = Regex::new(r"<[^>]*>|[^>]*</sup>|[0-9]*</div>|\{[^>]*\}|\[[^>]*\]|[^>]*</dfn>")
             .unwrap();
         let cleanup = Regex::new(r"[^>]*>|<[^>]*|&lt;[^>]*&gt;").unwrap();
-        let mut lines_b = document.select(&title_selector).map(|x| x.inner_html());
+        let lines_b = document.select(&title_selector).map(|x| x.inner_html());
 
         let mut left: Vec<String> = Vec::new();
         let mut right: Vec<String> = Vec::new();
