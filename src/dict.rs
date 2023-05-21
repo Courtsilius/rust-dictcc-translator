48ed771bd57d34b1b2cd4f890f64696717b13380
Pascal <courtsilius@protonmail.com>
2023-05-22 00:12:40 +0200

    wip changes to get a max for translations

--- src/dict.rs
+++ src/dict.rs
@@ -8,8 +8,8 @@ pub mod dict_mod {
 
     pub fn get_language(s: String) -> Language {
         match s.as_str() {
-            "en" => Language::new("en".to_string(), "Englisch".to_string()),
-            "de" => Language::new("de".to_string(), "Deutsch".to_string()),
+            "en" => Language::new("en".to_string(), "Englisch".to_string(), "English".to_string()),
+            "de" => Language::new("de".to_string(), "Deutsch".to_string(), "German".to_string()),
             _ => panic!("No valid language found."),
         }
     }
@@ -32,6 +32,13 @@ pub mod dict_mod {
         scraper::Html::parse_document(response)
     }
 
+    fn string_index(source: &String, pattern: &String) -> Vec<usize> {
+        source
+            .match_indices(pattern)
+            .map(|(i, _)| i)
+            .collect()
+    }
+
     fn filter(document: Html, translation_request: TranslationRequest) -> Translation {
         let language_selector = scraper::Selector::parse("td.td2").unwrap();
         let mut lang_lines: String = "".to_owned();
@@ -40,17 +47,22 @@ pub mod dict_mod {
             lang_lines.push_str(&item);
         });
 
-        let v_f: Vec<_> = lang_lines
-            .match_indices(translation_request.from().name())
-            .map(|(i, _)| i)
-            .collect();
-        let v_t: Vec<_> = lang_lines
-            .match_indices(translation_request.to().name())
-            .map(|(i, _)| i)
-            .collect();
+        let mut v_f = string_index(&lang_lines, translation_request.from().name());
+        let mut from_index: &usize = v_f.first().unwrap_or(&0);
 
-        let from_index = v_f.first().unwrap_or(&0);
-        let to_index = v_t.first().unwrap_or(&0);
+        let mut v_t = string_index(&lang_lines, translation_request.to().name());
+        let mut to_index: &usize = v_t.first().unwrap_or(&0);
+
+
+        if from_index == &0 {
+            v_f = string_index(&lang_lines, translation_request.from().alt_name());
+            from_index = v_f.first().unwrap_or(&0);
+        }
+
+        if to_index == &0 {
+            v_t = string_index(&lang_lines, translation_request.to().alt_name());
+            to_index = v_t.first().unwrap_or(&0);
+        }
 
         let mut from_is_first: bool = true;
         if to_index < from_index {
@@ -59,14 +71,30 @@ pub mod dict_mod {
 
         let title_selector = scraper::Selector::parse("td.td7nl").unwrap();
 
-        let lines = document.select(&title_selector).map(|x| x.inner_html());
+        let mut lines = document.select(&title_selector).map(|x| x.inner_html());
+        let mut all: String = "".to_owned();
+        lines.zip(1..1000).for_each(|(item, _number)| {
+            all.push_str(&item);
+        });
+
+        let v_max = string_index(&all, &"2".to_string());
+        let max_index: &usize = v_max.first().unwrap_or(&0);
+
+
+        if max_index == &0 {
+            v_f = string_index(&all, &"Partial Matches".to_string());
+            from_index = v_f.first().unwrap_or(&0);
+        }
+
+
         let re = Regex::new(r"<[^>]*>|[^>]*</sup>|[0-9]*</div>|\{[^>]*\}|\[[^>]*\]|[^>]*</dfn>")
             .unwrap();
         let cleanup = Regex::new(r"[^>]*>|<[^>]*|&lt;[^>]*&gt;").unwrap();
+        let mut lines_b = document.select(&title_selector).map(|x| x.inner_html());
 
         let mut left: Vec<String> = Vec::new();
         let mut right: Vec<String> = Vec::new();
-        lines.zip(1..1000).for_each(|(item, number)| {
+        lines_b.zip(1..*max_index).for_each(|(item, number)| {
             // didn't wanna figure out why it doesn't work with one pass only so leaving
             // it like this for now
             let first_pass = re.replace_all(&item, "");
